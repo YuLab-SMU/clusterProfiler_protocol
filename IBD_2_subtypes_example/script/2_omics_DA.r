9105a0c22a45ea9b63f93ddd1bca93485def2c80
luoxiao <778055611@qq.com>
2023-09-11 22:05:37 +0800

    Improve the code

--- IBD_2_subtypes_example/script/2_omics_DA.r
+++ IBD_2_subtypes_example/script/2_omics_DA.r
@@ -1,24 +1,25 @@
-#clinical info
+# loading pkg
 library(MicrobiotaProcess)
-#2 omics clinical info
-meta.mb <- read.csv("IBD_2_subtypes_example/input_data/metabolism_meta.csv", 
-                    header = T)
-meta.mg <- read.csv("IBD_2_subtypes_example/input_data/mg.meta.csv", header = T)
+# 2 omics clinical info
+meta_mb <- read.csv("IBD_2_subtypes_example/input_data/metabolism_meta.csv",
+                    header = TRUE)
+meta_mg <- read.csv("IBD_2_subtypes_example/input_data/mg.meta.csv",
+                    header = TRUE)
 
 # read 2 omics expression data
-metabolism <- read.csv("IBD_2_subtypes_example/input_data/metabolism_expr.csv", 
+metabolism <- read.csv("IBD_2_subtypes_example/input_data/metabolism_expr.csv",
                        row.names = 1,
-                       header = 1,
-                       check.name = F)
+                       header = TRUE,
+                       check.name = FALSE)
 metagenome <- read.csv("IBD_2_subtypes_example/input_data/mg.expr.csv",
                        row.names = 1,
-                       header = 1,
-                       check.name = F)
+                       header = TRUE,
+                       check.name = FALSE)
 
-#difference analysis function
+# difference analysis function
 DA <- function(expr, meta, DAgroup, filter_p = "fdr", filter_group) {
   mpse <- MPSE(expr)
-  mpse <- mpse %>% left_join(meta, by = c('Sample' = 'sample'))
+  mpse <- mpse %>% left_join(meta, by = c("Sample" = "sample"))
   result <- mpse %>%
     filter(Diagnosis %in% DAgroup) %>%
     mp_diff_analysis(
@@ -33,25 +34,28 @@ DA <- function(expr, meta, DAgroup, filter_p = "fdr", filter_group) {
     pull(OTU)
 }
 
-#Metagenomic differential analysis
-cd.mg <- DA(expr = metagenome, meta = meta.mg, DAgroup = c("Control", "CD"),  
-             filter_group = "CD",
-             filter_p = "pvalue")
-uc.mg <- DA(expr = metagenome, meta = meta.mg, DAgroup = c("Control", "UC"), 
-             filter_group = "UC",
-             filter_p = "pvalue")
-mg <- list(cd = cd.mg, uc = uc.mg)
+# Metagenomic differential analysis
+cd_mg <- DA(expr = metagenome, meta = meta_mg,
+            DAgroup = c("Control", "CD"),
+            filter_group = "CD",
+            filter_p = "pvalue")
+uc_mg <- DA(expr = metagenome, meta = meta_mg,
+            DAgroup = c("Control", "UC"),
+            filter_group = "UC",
+            filter_p = "pvalue")
+mg <- list(cd = cd_mg, uc = uc_mg)
 
-#Save the drawing data needed
+# Save the drawing data needed
 saveRDS(mg, "IBD_2_subtypes_example/result/IBD.gene.mpse.rds")
 
 
-#Metabolomic differential analysis
-cd.mb <- DA(expr = metabolism, meta = meta.mb, DAgroup = c("Control", "CD"), 
+# Metabolomic differential analysis
+cd_mb <- DA(expr = metabolism, meta = meta_mb,
+            DAgroup = c("Control", "CD"),
             filter_group = "CD")
-uc.mb <- DA(expr = metabolism, meta = meta.mb, DAgroup = c("Control", "UC"), 
+uc_mb <- DA(expr = metabolism, meta = meta_mb,
+            DAgroup = c("Control", "UC"),
             filter_group = "UC")
-mb <- list(cd = cd.mb, uc = uc.mb)
-
-#Save the drawing data needed
+mb <- list(cd = cd_mb, uc = uc_mb)
+# Save the drawing data needed
 saveRDS(mb, "IBD_2_subtypes_example/result/IBD.cpd.mpse.rds")

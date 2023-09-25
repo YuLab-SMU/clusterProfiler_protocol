7adf25f2c53ce840a21e853fa7c427e54b8630bc
luoxiao <778055611@qq.com>
2023-09-25 13:54:05 +0800

    update parameter's name.

--- IBD_2_subtypes_example/script/visualize.r
+++ IBD_2_subtypes_example/script/visualize.r
@@ -6,12 +6,12 @@ library(ggplot2)
 #IBD-only-gene
 dir <- "IBD_2_subtypes_example/result"
 de_gene <- readRDS(file.path(dir,"IBD.gene.mpse.rds"))
-gs <- compareCluster(geneClusters = de_gene,
+gene_enrich_result<- compareCluster(geneClusters = de_gene,
                      fun = "enrichKEGG",
                      organism = "ko")
 
 
-p1 <- dotplot(gs, facet='intersect', showCategory = 10, split = "intersect", label_format = 60) +
+p1 <- dotplot(gene_enrich_result, facet='intersect', showCategory = 10, split = "intersect", label_format = 60) +
   ggtitle("Functional enrichment of intestinal genes") + 
   theme(plot.title = element_text(hjust = 1))
 
@@ -22,12 +22,12 @@ ggsave(p1,
 
 #IBD-only-compound
 de_cpd <- readRDS(file.path(dir, "IBD.cpd.mpse.rds"))
-gs <- compareCluster(geneClusters = de_cpd,
+cpd_enrich_result <- compareCluster(geneClusters = de_cpd,
                      fun = "enrichKEGG",
                      organism = "cpd")
 
 
-p2 <- dotplot(gs, facet='intersect', showCategory = 10, split = "intersect", label_format = 60) +
+p2 <- dotplot(cpd_enrich_result, facet='intersect', showCategory = 10, split = "intersect", label_format = 60) +
   ggtitle("Functional enrichment of chemical compounds") +
   theme(plot.title = element_text(hjust = 1))
 
@@ -47,3 +47,15 @@ ggsave(fig,
        file = file.path(dir, "fig.png"),
        width = 13,
        height = 6.5)
+# gs1  <-  pairwise_termsim(gene_enrich_result)
+# # p3 <- 
+# treeplot(gs1, offset_tiplab = 16, 
+# showCategory = 15,label_format=80,
+# offset= rel(1.5)) + ggtitle("Functional enrichment of intestinal genes(clustered)")+
+# theme(plot.title = element_text(hjust = 0))
+
+# gs2  <-  pairwise_termsim(cpd_enrich_result)
+# # p4 <- ?
+# treeplot(gs2, offset_tiplab = 10, showCategory = 15,label_format=50) + 
+# ggtitle("Functional enrichment of chemical compounds(clustered)")+
+# theme(plot.title = element_text(hjust = 0))

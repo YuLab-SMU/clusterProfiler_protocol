c34e34a57c183d9b3628396f646c3dfb6381c093
Zijing Xie <xzj14035@outlook.com>
2023-10-09 22:21:15 +0800

    增加了记时代码

--- single_cell_example/script/pbmc_single_cell_example.R
+++ single_cell_example/script/pbmc_single_cell_example.R
@@ -1,3 +1,7 @@
+input_dir <- "single_cell_example/input_data"
+library(tictoc)
+tic("single cell example total")
+tic("setup the environment and data objects")
 library(Seurat)
 library(CelliD)
 library(clusterProfiler)
@@ -6,96 +10,89 @@ library(ggrepel)
 library(ggsc)
 library(patchwork)
 
-input_dir <- "single_cell_example/input_data"
-# import cell marker database
+pbmc_counts <- Read10X(
+  data.dir = file.path(input_dir, "filtered_gene_bc_matrices/hg19/")
+)
+pbmc <- CreateSeuratObject(counts = pbmc_counts, project = "pbmc3k",
+                           min.cells = 3
+)
+toc()
+
+tic("Data preprocessing workflow")
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+pbmc <- subset(pbmc,
+               subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5
+)
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize",
+                      scale.factor = 10000
+)
+pbmc <- ScaleData(pbmc)
+toc()
+
+tic("Dimensionality reduction")
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst",
+                             nfeatures = 2000
+)
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+toc()
+
+tic("Cluster cells and identify markers of cell clusters")
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+pbmc <- RunMCA(pbmc)
+cluster_markers <- GetGroupGeneSet(X = pbmc,
+                                   group.by = "seurat_clusters",
+                                   n.features = 20
+)
+toc()
+
+tic("cell type annotation")
 cell_marker_db <- read.gmt(
   file.path(input_dir, "cell_marker_db/c8.all.v2023.1.Hs.symbols.gmt")
 )
-# Load the PBMC dataset
-pbmc_data <- Read10X(
-  data.dir = file.path(input_dir, "filtered_gene_bc_matrices/hg19/")
-)
-# Initialize the Seurat object with the raw (non-normalized data).
-pbmc <- CreateSeuratObject(counts = pbmc_data, project = "pbmc3k",
-  min.cells = 3
-)
-# Preprocess
-pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
-pbmc <- subset(pbmc,
-  subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5
-)
-# Normalizing the data
-pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize",
-  scale.factor = 10000
-)
-
-# Scaling the data
-all.genes <- rownames(pbmc)
-pbmc <- ScaleData(pbmc, features = all.genes)
-
-# Identification of highly variable features
-pbmc <- FindVariableFeatures(pbmc, selection.method = "vst",
-  nfeatures = 2000
-)
-# Cluster the cells
-pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
-pbmc <- RunUMAP(pbmc, dims = 1:10)
-pbmc <- FindNeighbors(pbmc, dims = 1:10)
-pbmc <- FindClusters(pbmc, resolution = 0.5)
-
-# Find cluster biomarkers
-pbmc <- RunMCA(pbmc)
-cluster_markers <- GetGroupGeneSet(X = pbmc,
-  group.by = "seurat_clusters",
-  n.features = 20
-)
-
-# cell type annotate using default method
-seurat_cluster_id <- c("Naive CD4 T", "CD14+ Mono", "Memory CD4 T", "B",
-  "CD8 T", "FCGR3A+ Mono", "NK", "DC", "Platelet"
-)
-names(seurat_cluster_id) <- levels(pbmc)
-seurat_pbmc <- RenameIdents(pbmc, seurat_cluster_id)
-seurat_pbmc_plot <- DimPlot(seurat_pbmc, label = TRUE, repel = TRUE) +
-  theme(legend.position = "none")
-
-
-
-# predict cell type using clusterProfiler
 cell_type_enrich_result <- compareCluster(cluster_markers,
-  fun = "enricher",
-  TERM2GENE = cell_marker_db
+                                          fun = "enricher",
+                                          TERM2GENE = cell_marker_db
 )
 
-# cell type annotate using clusterProfiler
 predict_cell_type <- function(enrich_result) {
   enrich_result <- as.data.frame(enrich_result)
   result <- split(
     enrich_result, enrich_result$Cluster
   ) |>
-    vapply(function(x) {
-      x$ID[which.min(x$p.adjust)]
-    }, FUN.VALUE = character(1))
+    sapply(function(x) {
+      x$ID[which.min(x$qvalue)]
+    })
   cell_type <- gsub("_", " ", result) |>
     yulab.utils::str_wrap(18)
   names(cell_type) <- names(result)
   return(cell_type)
 }
-
 cell_type_predict <- predict_cell_type(cell_type_enrich_result)
 clusterprofiler_pbmc <- RenameIdents(pbmc, cell_type_predict)
-
 clusterprofiler_pbmc_plot <- sc_dim(clusterprofiler_pbmc) +
-  sc_dim_geom_label(geom = geom_text_repel, color = "black",
+  sc_dim_geom_label(geom = ggrepel::geom_text_repel, color = "black",
                     bg.color = "white") +
   theme(legend.position = "none")
+ggsave(clusterprofiler_pbmc_plot,
+       file = "single_cell_example/result/figure4.pdf",
+       width = 12, height = 7)
+toc()
+toc()
 
 # compare two methods
 library(aplot)
+# cell type annotate using default method
+seurat_cluster_id <- c("Naive CD4 T", "CD14+ Mono", "Memory CD4 T", "B",
+                       "CD8 T", "FCGR3A+ Mono", "NK", "DC", "Platelet"
+)
+names(seurat_cluster_id) <- levels(pbmc)
+seurat_pbmc <- RenameIdents(pbmc, seurat_cluster_id)
+seurat_pbmc_plot <- DimPlot(seurat_pbmc, label = TRUE, repel = TRUE) +
+  theme(legend.position = "none")
 fig <- seurat_pbmc_plot | clusterprofiler_pbmc_plot +
   plot_annotation(tag_levels = "A")
-
-
 ggsave(fig, file = "single_cell_example/result/DimPlot.pdf",
   width = 12, height = 7
 )

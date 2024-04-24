e17c8b4bfc3638b3bc860c0c2f2690a38efef1d0
xiezijing <xzj14035@outlook.com>
2024-04-24 22:52:32 +0800

    update github pages

--- render.sh
+++ render.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 Rscript -e 'rmarkdown::render("index.Rmd")'
+Rscript -e 'rmarkdown::render("IBD_index.Rmd", output_file = "index", output_dir = "examples/IBD_2_subtypes")'
+Rscript -e 'rmarkdown::render("Phyllostachys_heterocycla_index.Rmd", output_file = "index", output_dir = "examples/Phyllostachys_heterocycla")'
+Rscript -e 'rmarkdown::render("single_cell_index.Rmd", output_file = "index", output_dir = "examples/single_cell")'
+Rscript -e 'rmarkdown::render("annot_data_index.Rmd", output_file = "index", output_dir = "examples/Phyllostachys_heterocycla/annot_data")'
+Rscript -e 'rmarkdown::render("cell_marker_db_index.Rmd", output_file = "index", output_dir = "examples/single_cell/cell_marker_db")'
+Rscript -e 'rmarkdown::render("filtered_gene_bc_matrices.Rmd", output_file = "index", output_dir = "examples/single_cell/filtered_gene_bc_matrices")'
+Rscript -e 'rmarkdown::render("hg19_index.Rmd", output_file = "index", output_dir = "examples/single_cell/filtered_gene_bc_matrices/hg19")'
